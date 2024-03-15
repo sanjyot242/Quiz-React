6c29146202bb3db9fc8f1b92fdcccedbaca7bc13
Sanjyot <41758207+sanjyot242@users.noreply.github.com>
2024-03-15 13:07:07 -0700

    Added Logic for Shuffling Answers and Summary Page

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,22 +6,39 @@ export default function Quiz() {
 
   const activeQuestionIndex = userAnswers.length;
 
+  const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
+
   function handleAnsSelect(selectedAnswer) {
     setUserAnswers((prevUserAns) => {
       return [...prevUserAns, selectedAnswer];
     });
   }
 
+  if (isQuizComplete) {
+    return (
+      <div>
+        <h1>Summary Page</h1>
+        <p>Quiz Summary</p>
+      </div>
+    );
+  }
+
+  const shuffledOptions = QUESTIONS[activeQuestionIndex].answers.sort(
+    () => Math.random() - 0.5
+  );
+
   return (
-    <div id='question'>
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id='answers'>
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li className='answer' key={answer}>
-            <button onClick={() => handleAnsSelect(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {shuffledOptions.map((answer) => (
+            <li className='answer' key={answer}>
+              <button onClick={() => handleAnsSelect(answer)}>{answer}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
